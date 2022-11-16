2ca82aace95b47537b8f0ba3c7757111186bd9cb
Jake Stewart <jakestewart711@icloud.com>
2022-11-16 09:50:12 -0500

    Fixed deadzone problems

--- TimeBase/src/main/java/frc/robot/Robot.java
+++ TimeBase/src/main/java/frc/robot/Robot.java
@@ -13,12 +13,9 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.XboxController;
-<<<<<<< HEAD
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-=======
->>>>>>> 4564c298b5c6578ab86520a08f0494ccb8d835ba
-
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -107,7 +104,8 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit() {}
 
-  /** This function is called periodically during operator control. */
+  /** This function is called periodically during operator control. 
+   * @return */
   @Override
   public void teleopPeriodic() {
     double lefttrain = xcontroll.getLeftY();
@@ -117,10 +115,10 @@ public class Robot extends TimedRobot {
     boolean invert = xcontroll.getStartButton();
     boolean revinmid = xcontroll.getYButton();
     boolean revmidupshoot = xcontroll.getXButton();
-    if (deadzone(intake) != 0) {
+    if (Math.abs(intake) > .15) {
       /** Turn on the intake/midtake */
     }
-    if (deadzone(miduptakeshooter) != 0) {
+    if (Math.abs(miduptakeshooter) > .15) {
       /** Midtake, uptake, and shooter */
     }
     if (invert == true) {
@@ -132,20 +130,20 @@ public class Robot extends TimedRobot {
     if (revmidupshoot == true) {
       /** Reverse midtake, uptake, and shooter */
     }
-    if (deadzone(lefttrain) != 0) {
+    if (Math.abs(lefttrain) > .15) {
       frontLeftMotor.set(lefttrain);
-      backLeftMotor.set(lefttrain)
+      backLeftMotor.set(lefttrain);
     }
-    if (deazone(righttrain) != 0) {
+    if (Math.abs(righttrain) > .15) {
       frontRightMotor.set(righttrain);
       backRightMotor.set(righttrain);
     }
-    public double deadzone(double doubleArgument) {
-      if(Math.abs(doubleArgument) < .15) return 0;
-      else return doubleArgument;
-    }
   }
   
+
+
+
+
   /** This function is called once when the robot is disabled. */
   @Override
   public void disabledInit() {}
