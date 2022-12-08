d06f3c3634d65f414e16f5c3368c7ac3d983cbe5
Jake Stewart <jakestewart711@icloud.com>
2022-12-08 15:53:49 -0500

    Acctually added motor nums this time

--- CommandBase/src/main/java/frc/robot/Constants.java
+++ CommandBase/src/main/java/frc/robot/Constants.java
@@ -13,5 +13,11 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    public static final int INTAKE_PORT = 0;
+    public static final int INTAKE_PORT = 4;
+    public static final int FRONT_LEFT_MOTOR_PORT = 2;
+    public static final int FRONT_RIGHT_MOTOR_PORT = 3;
+    public static final int BACK_LEFT_MOTOR_PORT = 0;
+    public static final int BACK_RIGHT_MOTOR_PORT = 1;
+    public static final int SHOOTER_MOTOR_PORT = 15;
+
 }
