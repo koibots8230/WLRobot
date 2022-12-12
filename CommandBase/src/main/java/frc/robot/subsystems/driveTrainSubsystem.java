ff08cafb09e0113238612134dfcea7a011d08881
redguy999 <bigredguy05@hotmail.com>
2022-12-12 09:52:54 -0500

    fixes the errors in the driveTrain subsystem.

--- CommandBase/src/main/java/frc/robot/subsystems/driveTrainSubsystem.java
+++ CommandBase/src/main/java/frc/robot/subsystems/driveTrainSubsystem.java
@@ -7,31 +7,47 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 //This drivetrain code is built in accordance with last year's tankdrive drivetrain.
 public class driveTrainSubsystem extends SubsystemBase {
-    private final TalonSRX leftMotor;
-    private final TalonSRX rightMotor;
-    public float speed = 50; //Assuming speed is in percentage.
+    private final TalonSRX frontleftMotor;
+    private final TalonSRX backleftMotor;
+    private final TalonSRX frontrightMotor;
+    private final TalonSRX backrightMotor;//right and left motors should move together.
+    
+    public double speed = 0.5; //Assuming speed is in percentage.
+    //FYI: 1: double is just a better float. 2: speed is a percentage but as a demical.
     public driveTrainSubsystem() {
-        TalonSRX rightMotor = new TalonSRX();
-        TalonSRX leftMotor = new TalonSRX();
+        frontleftMotor = new TalonSRX(2);
+        backleftMotor = new TalonSRX(0);
+        frontrightMotor = new TalonSRX(3);
+        backrightMotor = new TalonSRX(1);
     }
     public void driveFwd() { // Function to drive forward at speed.
-        rightMotor.set(ControlMode.PercentOutput, speed);
-        leftMotor.set(ControlMode.PercentOutput, speed);
+        backrightMotor.set(ControlMode.PercentOutput, speed);
+        frontrightMotor.set(ControlMode.PercentOutput, speed);
+        frontleftMotor.set(ControlMode.PercentOutput, speed);
+        backleftMotor.set(ControlMode.PercentOutput, speed);
     }
     public void driveBkwd() { // Function to drive backward at speed.
-        rightMotor.set(ControlMode.PercentOutput, -speed);
-        leftMotor.set(ControlMode.PercentOutput, -speed);
+        backrightMotor.set(ControlMode.PercentOutput, -speed);
+        frontrightMotor.set(ControlMode.PercentOutput, -speed);
+        frontleftMotor.set(ControlMode.PercentOutput, -speed);
+        backleftMotor.set(ControlMode.PercentOutput, -speed);
     }
     public void turnRight() { // Turn right at speed. Differential turning.
-        rightMotor.set(ControlMode.PercentOutput,-speed);
-        leftMotor.set(ControlMode.PercentOutput, speed);
+        backrightMotor.set(ControlMode.PercentOutput, -speed);
+        frontrightMotor.set(ControlMode.PercentOutput, -speed);
+        frontleftMotor.set(ControlMode.PercentOutput, speed);
+        backleftMotor.set(ControlMode.PercentOutput, speed);
     }
     public void turnLeft() { // Turn left at speed. Differential turning.
-        rightMotor.set(ControlMode.PercentOutput, speed);
-        leftMotor.set(ControlMode.PercentOutput, -speed);
+        backrightMotor.set(ControlMode.PercentOutput, speed);
+        frontrightMotor.set(ControlMode.PercentOutput, speed);
+        frontleftMotor.set(ControlMode.PercentOutput, -speed);
+        backleftMotor.set(ControlMode.PercentOutput, -speed);
     }
     public void stopDriving() { // Brings the robot to a standstill.
-        rightMotor.set(ControlMode.PercentOutput, 0);
-        leftMotor.set(ControlMode.PercentOutput, 0);
+        backrightMotor.set(ControlMode.PercentOutput, 0);
+        frontrightMotor.set(ControlMode.PercentOutput, 0);
+        frontleftMotor.set(ControlMode.PercentOutput, 0);
+        backleftMotor.set(ControlMode.PercentOutput, 0);
     }
 }
